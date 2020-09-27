68edeb470e78967eeb97cb0cebf9117bced757a3
govijay <vijayg17@icloud.com>
2020-09-27 03:48:31 -0700

    DropDown Component Sync
    DropDown Component Sync

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
 function SearchParams() {
-  const location = "San Francisco, CA";
+  const [location, setLocation] = useState("San Francisco, CA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropDown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropDown] = useDropdown("Breed", "", breeds);
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
-          <button>Submit</button>
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </label>
+        <AnimalDropDown />
+        <BreedDropDown />
+        <button>Submit</button>
       </form>
     </div>
   );
